888f437a50d979dcca69374e5eb56811b31a93f4
jayxto <vz85j2jgb7@privaterelay.appleid.com>
2026-09-08 21:40:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const http = require('http');
 const { Server } = require('socket.io');
 
 const app = express();
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
 const server = http.createServer(app);
 const io = new Server(server);
 
